53e1389a51cdfd217a836723b673a65614c29eee
Owen Schwartz <owen@txv.io>
2025-01-01 21:37:16 -0500

    Fix link

--- packages/docusaurus/docusaurus.config.ts
+++ packages/docusaurus/docusaurus.config.ts
@@ -96,7 +96,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/intro',
+              to: '/docs/overview',
             },
           ],
         },
