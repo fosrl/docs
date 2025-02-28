f9b45ffa61197fc10b352c78db1bf40fa85c69b9
Owen <owen@txv.io>
2025-02-27 19:12:13 -0500

    Use ` because of the JS string interpolation

--- packages/docusaurus/src/components/WgetQuickInstaller.tsx
+++ packages/docusaurus/src/components/WgetQuickInstaller.tsx
@@ -9,7 +9,7 @@ const WgetQuickInstall: React.FC = () => {
         (async () => {
             const release = await fetchLatestRelease("fosrl/pangolin");
             setText(
-                'wget -O installer "https://github.com/fosrl/pangolin/releases/download/${release}/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer'
+                `wget -O installer "https://github.com/fosrl/pangolin/releases/download/${release}/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer`
             );
         })();
     }, []);
